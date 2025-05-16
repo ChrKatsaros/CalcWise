3dea50b0c93e6ebe89aa1508a9ca9551e9c8bf66
ChrKatsaros <chr.katsaros99@gmail.com>
2025-05-16 12:42:20 +0300

    Update vite.config.js

--- vite.config.js
+++ vite.config.js
@@ -4,5 +4,5 @@ import react from '@vitejs/plugin-react'
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
-  base: '/CalcWise/', // Αυτό είναι το όνομα του repository σου στο GitHub
+  base: './', 
 })
